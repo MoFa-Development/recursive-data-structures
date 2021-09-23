de89bed1bcdfc805f9cc53f9fe2728de5d4a06bd
Antricks <33262801+Antricks@users.noreply.github.com>
2021-09-23 15:31:18 +0200

    remove last BlueJ files including docs folder

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"DataStructures.CataloguedChainUpDown"},{"l":"DataStructures.Chain"},{"l":"DataStructures.ChainUp"},{"l":"Exceptions"}]
